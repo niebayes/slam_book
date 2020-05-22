284667210bc65437e5e7fd9934ededf1ae2de0a5
niebayes <niebayes@gmail.com>
2020-05-22 21:12:01 +0800

    add a new feature: plot_curve

--- src/ch6/my_codes/handwriting_gauss_newton.cc
+++ src/ch6/my_codes/handwriting_gauss_newton.cc
@@ -11,6 +11,8 @@
 
 #include "Eigen/Dense"  // Eigen::Matrix3d, Eigen::Vector3d, Eigen::MatrixBase.ldlt(), Eigen::MatrixType.solve()
 #include "opencv2/core.hpp"  // cv::RNG
+#include "opencv2/highgui.hpp"
+#include "opencv2/plot.hpp"
 
 using namespace std;
 
@@ -37,15 +39,16 @@ class GaussNewton {
     return y;
   }
 
-  dataset generate_data(const int dataset_size, const double sigma) {
+  dataset generate_data(vd &beta, const int dataset_size, const double sigma,
+                        bool additive_noise) {
     // * Generate dataset according to the given params beta.
     // * The generated dataset was corruptd by gaussian noise.
     dataset data;
-    vd beta{1., 2., 1.};  // beta: given params for generate data.
     cv::RNG rng;
     for (int i = 0; i < dataset_size; ++i) {
       double x = i / 100.;
-      double y = func(beta, x) + rng.gaussian(sigma);
+      double y =
+          additive_noise ? func(beta, x) + rng.gaussian(sigma) : func(beta, x);
       data.emplace_back(x, y);
     }
     return data;
@@ -59,6 +62,7 @@ class GaussNewton {
     double cost = 0;
 
     // TODO Use std::accumulate to refactor this loop.
+    // ? Does std::accumulate refactoring work doable?
     for (auto it = data.begin(); it < data.end(); ++it) {
       auto x = it->first, y = it->second;
       double residual = y - func(beta, x);
@@ -78,19 +82,37 @@ class GaussNewton {
   }
 };
 
-int main(int argc, char *argv[]) {
-  s_clock::time_point t1 = s_clock::now();
+void plot_curve(dataset &data) {
+  vd data_x, data_y;
+  for (auto &p : data) {
+    data_x.push_back(p.first);
+    data_y.push_back(p.second);
+  }
+  auto curve = cv::plot::Plot2d::create(data_x, data_y);
+  cv::Mat image;
+  curve->render(image);
+  cv::imshow("Function curve", image);
+  cv::waitKey(0);
+}
 
+int main(int argc, char *argv[]) {
   // Gauss-Newton initialization
   GaussNewton gn;
+  vd given_beta{1., 2., 1.};  // beta: given params for generate data.
   double sigma = 1.;
+  bool additive_noise = true;
   int dataset_size = 100;
-  auto data = gn.generate_data(dataset_size, sigma);
+  auto data = gn.generate_data(given_beta, dataset_size, sigma, additive_noise);
   int iterations = 20;
   double last_cost = 0.;
 
+  // Plot the original function curve.
+  plot_curve(data);
+
   // Estimands
   vd beta{2., -1., 5.};
+
+  s_clock::time_point t1 = s_clock::now();
 
   // Gauss-Newton starts
   for (int iter = 0; iter < iterations; ++iter) {
@@ -108,6 +130,11 @@ int main(int argc, char *argv[]) {
 
   s_clock::time_point t2 = s_clock::now();
   m_secs time_span = chrono::duration_cast<m_secs>(t2 - t1);
+
+  // Plot the estimated function curve.
+  additive_noise = false;
+  data = gn.generate_data(beta, dataset_size, sigma, additive_noise);
+  plot_curve(data);
 
   // Post-processing
   cout << "Time consumed: " << time_span.count() << " milliseconds" << endl;
