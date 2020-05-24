790901348f70cfa9bab44604a611a06f623a6530
niebayes <niebayes@gmail.com>
2020-05-24 22:01:44 +0800

    commit test

---
+++ git_commit_reset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git filter-branch --env-filter '
+OLD_EMAIL="nie_bayes@gmail.com"
+OLD_EMAIL2="870530032@qq.com"
+OLD_EMAIL3="lzx@nie-bayes.local"
+OLD_NAME="Nie Shicong"
+OLD_NAME2="nie_bayes"
+OLD_NAME3="Bayes Nie"
+OLD_NAME4="lzx"
+CORRECT_NAME="niebayes"
+CORRECT_EMAIL="niebayes@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ] || [ "GIT_COMMITTER_NAME" = "$OLD_NAME2" ] || [ "GIT_COMMITTER_NAME" = "$OLD_NAME3" ] || [ "GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ] || [ "GIT_COMMITTER_NAME" = "$OLD_NAME4" ] || [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL3" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ] || [ "GIT_AUTHOR_NAME" = "$OLD_NAME2" ] || [ "GIT_AUTHOR_NAME" = "$OLD_NAME3" ] || [ "GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ] || [ "GIT_AUTHOR_NAME" = "$OLD_NAME4" ] || [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL3" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi' -f --tag-name-filter cat -- --branches --tags
--- src/ch6/my_codes/ceres_curve_fitting.cc
+++ src/ch6/my_codes/ceres_curve_fitting.cc
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 #include "ceres/ceres.h"
+#include "opencv2/core.hpp"
 // #include "glog/logging.h"  // Google Logging Library
 
 using ceres::AutoDiffCostFunction;
@@ -9,37 +11,57 @@ using ceres::Problem;
 using ceres::Solve;
 using ceres::Solver;
 
-struct CostFunctor {
+using cv::RNG;
+
+class CostFunctor {
+ public:
+  CostFunctor(double x, double y) : x_(x), y_(y) {}
+
   template <typename T>
-  bool operator()(const T* const x, T* residual) const {
-    residual[0] = 10. - x[0];
+  bool operator()(const T* const a, const T* const b, const T* const c,
+                  T* residual) const {
+    residual[0] =
+        T(y_) - ceres::exp(a[0] * T(x_) * T(x_) + b[0] * T(x_) + c[0]);
     return true;
   }
+
+ private:
+  double x_, y_;
 };
 
-int main(int argc, char* argv[]) {
-  google::InitGoogleLogging(argv[0]);
+int main(int argc, char** argv) {
+  double real_a = 1., real_b = 2., real_c = 1.;
+  double initial_a = 2., initial_b = -1., initial_c = 5.;
+  int kN = 100;
+  double sigma = 1.;
+  double inv_sigma = 1. / sigma;
+  RNG rng;
 
-  // The variable to solve for with its initial value. It will be
-  // mutated in place by the solver.
-  double x = 0.5;
-  const double initial_x = x;
+  std::vector<double> data_x, data_y;
+  for (int i = 0; i < kN; ++i) {
+    double x = i / 100.;
+    data_x.push_back(x);
+    data_y.push_back(exp(real_a * x * x + real_b * x + real_c) +
+                     rng.gaussian(sigma));
+  }
 
-  // Build the problem.
   Problem problem;
+  double a = initial_a, b = initial_b, c = initial_c;
+  for (int i = 0; i < kN; ++i) {
+    CostFunction* cost_function =
+        new AutoDiffCostFunction<CostFunctor, 1, 1, 1, 1>(
+            new CostFunctor(data_x[i], data_y[i]));
+    problem.AddResidualBlock(cost_function, nullptr, &a, &b, &c);
+  }
 
-  // Set up the only cost function (also known as residual). This uses
-  // auto-differentiation to obtain the derivative (jacobian).
-  CostFunction* cost_function =
-      new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
-  problem.AddResidualBlock(cost_function, NULL, &x);
-
-  // Run the solver!
   Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_NORMAL_CHOLESKY;
   options.minimizer_progress_to_stdout = true;
   Solver::Summary summary;
   Solve(options, &problem, &summary);
+
   std::cout << summary.BriefReport() << "\n";
-  std::cout << "x : " << initial_x << " -> " << x << "\n";
+  std::cout << "Estimated params are: \n";
+  std::cout << a << " " << b << " " << c;
   return 0;
 }
